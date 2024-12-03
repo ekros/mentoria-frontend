1476fa9dd10dd294b4667e338ef943484d27337f
Eric Ros <ericrosbh@gmail.com>
2024-12-03 17:21:21 +0100

    fix text in exercise

--- typescript-primitives/example3.ts
+++ typescript-primitives/example3.ts
@@ -1,8 +1,7 @@
 
 // functions
 // 1. What do you think the type "any" is doing?
-// 2. Pass the correct types
-// 3. is there something wrong with the type of "result"? Fix it.
+// 2. Pass some parameter to the toBoolean function (try different values and run the program)
 
 const toBoolean = (value: any): boolean => {
     return !!value;

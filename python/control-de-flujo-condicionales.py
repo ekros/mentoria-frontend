22bccb4a758ea4e2bdb7b7f370c37915614dfff4
Eric Ros <ekros@outlook.com>
2025-11-19 20:10:44 +0100

    Add first python exercise

---
+++ python/control-de-flujo-condicionales.py
@@ -0,0 +1,10 @@
+# Ejercicios Condicionales
+
+
+# escribe una sentencia if que escriba "Temperatura fuera del rango cómodo" cuando la temperatura es menor a 10 o mayor a 35.
+
+temperatura = 5
+
+# escribe tu if aquí
+
+print("Temperatura fuera del rango cómodo")
